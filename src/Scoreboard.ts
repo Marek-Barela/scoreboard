467d6d80df8e6d49514ae39018fe4fa0291e21c1
Marek Bareła <marek.barela@wp.pl>
2025-06-11 18:07:48 +0200

    feat: implement startMatch and getSummary methods in Scoreboard

--- src/Scoreboard.ts
+++ src/Scoreboard.ts
@@ -1,1 +1,25 @@
-export class Scoreboard {}
+type Match = {
+  homeTeam: string;
+  awayTeam: string;
+  homeScore: number;
+  awayScore: number;
+};
+
+export class Scoreboard {
+  private matches: Match[] = [];
+
+  startMatch(homeTeam: string, awayTeam: string) {
+    const match: Match = {
+      homeTeam,
+      awayTeam,
+      homeScore: 0,
+      awayScore: 0,
+    };
+
+    this.matches.push(match);
+  }
+
+  getSummary() {
+    return this.matches;
+  }
+}
